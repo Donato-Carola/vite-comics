733d6e4e857cd110d5c0fde912f635593db19302
Donato-Carola <don94@live.it>
2023-12-14 15:55:39 +0100

    aggiunta file scss e img

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,77 @@
+<template lang="">
+    <header>
+   <div class='logo'>
+    
+   </div>
+   <nav>
+    <ul>
+        <li v-for="list in navList" >
+         {{list.name}}
+        </li>
+    </ul>
+   </nav>
+   
+</header>
+</template>
+
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      navList: [
+        {
+          name: 'characters',
+          active: false,
+        },
+        {
+          name: 'comics',
+          active: false,
+        },
+        {
+          name: 'movies',
+          active: false,
+        },
+        {
+          name: 'tv',
+          active: false,
+        },
+        {
+          name: 'games',
+          active: false,
+        },
+        {
+          name: 'collectibles',
+          active: false,
+        },
+        {
+          name: 'videos',
+          active: false,
+        },
+        {
+          name: 'fans',
+          active: false,
+        },
+        {
+          name: 'news',
+          active: false,
+        },
+        {
+          name: 'shop',
+          active: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+ul{
+  list-style: none;
+}
+</style>
